eb0475c9a3073edfd9d02cb0d039f45d7374ca75
NoorulAmeenJ <noorularafat786@gmail.com>
2023-01-22 21:38:29 +0530

    day1 task pending

--- src/App.js
+++ src/App.js
@@ -4,9 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+        <Card/>
+        <Card/>
+        <Card/>
     </div>
   );
 }
+
+function Card(){
+  return(
+      <div className="card">
+        <div className="inside header">title</div>
+        <div className="inside rate">amount</div>
+        <hr></hr>
+        <ul>
+          <li>Single User</li>
+          <li>5GB Storage</li>
+          <li>Unlimited Public Projects</li>
+          <li>Community Access</li>
+          <li>Unlimited Private Projects</li>
+          <li>Dedicated Phone Support</li>
+          <li>Free Subdomain</li>
+          <li>Monthly Status Reports</li>
+        </ul>
+        <button>Button</button>
+
+        </div>
+  )
+}
+
 
 export default App;
