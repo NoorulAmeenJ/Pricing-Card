60a0a71038b69dab77470ea675f508bb312901c1
NoorulAmeenJ <noorularafat786@gmail.com>
2023-01-23 10:59:24 +0530

    outloook completed

--- src/App.js
+++ src/App.js
@@ -2,30 +2,80 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let values = [ 
+    {
+             item: "Free",
+             amount: "$0/month",
+             user: "Single User",
+             storage: "5GB Storage",
+             public: "Unlimited Public Projects",
+             community:"Community Access",
+             private: "Unlimited Private Projects",
+             phone: "Dedicated Phone Support",
+             domain: "Free Subdomain",
+             report: "Monthly Status Reports"
+    },
+    {
+      item: "PLUS",
+      amount: "$9/month",
+      user: "5 Users",
+      storage: "50GB Storage",
+      public: "Unlimited Public Projects",
+      community:"Community Access",
+      private: "Unlimited Private Projects",
+      phone: "Dedicated Phone Support",
+      domain: "Free Subdomain",
+      report: "Monthly Status Reports"
+},
+{
+  item: "PRO",
+  amount: "$49/month",
+  user: "Unlimited Users",
+  storage: "150GB Storage",
+  public: "Unlimited Public Projects",
+  community:"Community Access",
+  private: "Unlimited Private Projects",
+  phone: "Dedicated Phone Support",
+  domain: "Free Subdomain",
+  report: "Monthly Status Reports"
+}
+  ]
   return (
     <div className="App">
-        <Card/>
-        <Card/>
-        <Card/>
+      {values.map( (eachone,id) =>(
+        <Card item={eachone.item} 
+        amount={eachone.amount} 
+        user={eachone.user}
+        storage={eachone.storage}
+        public={eachone.public}
+        community={eachone.community}
+        private={eachone.private}
+        phone={eachone.phone}
+        domain={eachone.domain}
+        report={eachone.report}
+         />
+      ) )}
+        
+       
     </div>
   );
 }
 
-function Card(){
+function Card(props){
   return(
       <div className="card">
-        <div className="inside header">title</div>
-        <div className="inside rate">amount</div>
+        <div className="inside header">{props.item}</div>
+        <div className="inside rate"><h1> {props.amount}</h1></div>
         <hr></hr>
         <ul>
-          <li>Single User</li>
-          <li>5GB Storage</li>
-          <li>Unlimited Public Projects</li>
-          <li>Community Access</li>
-          <li>Unlimited Private Projects</li>
-          <li>Dedicated Phone Support</li>
-          <li>Free Subdomain</li>
-          <li>Monthly Status Reports</li>
+          <li>{props.user}</li>
+          <li>{props.storage}</li>
+          <li>{props.public}</li>
+          <li>{props.community}</li>
+          <li>{props.private}</li>
+          <li>{props.phone}</li>
+          <li>{props.domain}</li>
+          <li>{props.report}</li>
         </ul>
         <button>Button</button>
 
